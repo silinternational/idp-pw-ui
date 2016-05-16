9620e476ff933428c23dcd130a2619e4d57b0a36
Billy Clark <billy_clark@sil.org>
2016-05-16 12:48:26 -0400

    someone would have to be fiddling with app to get to this page, besides, api won't allow a reset for an authenticated user.

--- app/forgot/forgot.controller.js
+++ app/forgot/forgot.controller.js
@@ -24,7 +24,6 @@
         //////////////////////////////////////////////////////////////////
 
         function activate() {
-//TODO: if the user is logged in send them to change
         }
 
         function recaptchaAnswered(response) {
