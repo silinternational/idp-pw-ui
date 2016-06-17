5cf5a8131189f594bc708fd14e46ea04323f63c5
Billy Clark <billy_clark@sil.org>
2016-06-16 21:29:31 -0400

    realized these didn't belong in the same file.

---
+++ app/data/progress-indicator.config.js
@@ -0,0 +1,50 @@
+(function () {
+    'use strict';
+
+    angular
+      .module('password.data')
+      .config(configureProgressInterceptor);
+
+    function configureProgressInterceptor($httpProvider) {
+        $httpProvider.interceptors.push(progressInterceptor);
+
+        //////////////////////////////////////////////////////////////////
+
+        function progressInterceptor($q, $injector) {
+            return {
+                'request': requestHandler,
+                'response': responseHandler,
+                'responseError': errorHandler
+            };
+
+            //////////////////////////////////////////////////////////////
+
+            function requestHandler(config) {
+                // wanted to simply inject service normally but it
+                // caused a circular dependency with $http
+                // :-( TODO: would love to see this corrected
+                $injector
+                  .get('progressIndicatorService')
+                  .queue(config.url);
+
+                return config;
+            }
+
+            function responseHandler(response) {
+                $injector
+                  .get('progressIndicatorService')
+                  .dequeue(response.config.url);
+
+                return response;
+            }
+
+            function errorHandler(rejection) {
+                $injector
+                  .get('progressIndicatorService')
+                  .dequeue(rejection.config.url);
+
+                return $q.reject(rejection);
+            }
+        }
+    }
+})();
