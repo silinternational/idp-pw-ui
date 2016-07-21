61ef0b708a0c5ac78914ce8a2ce854e279f7eeea
Billy Clark <billy_clark@sil.org>
2016-07-21 15:45:55 -0400

    users were not able to change passwords even when valid because inappropriate validators were being added indiscriminately

--- app/change/constraints.directive.js
+++ app/change/constraints.directive.js
@@ -20,11 +20,25 @@
         }
 
         function addValidators(scope, el, attrs, ngModel) {
-            ngModel.$validators.minLength = minLength;
-            ngModel.$validators.maxLength = maxLength;
-            ngModel.$validators.minNum = minNum;
-            ngModel.$validators.minSpecial = minSpecial;
-            ngModel.$validators.minUpper = minUpper;
+            if (config.password.minLength) {
+                ngModel.$validators.minLength = minLength;
+            }
+
+            if (config.password.maxLength) {
+                ngModel.$validators.maxLength = maxLength;
+            }
+
+            if (config.password.minNum) {
+                ngModel.$validators.minNum = minNum;
+            }
+
+            if (config.password.minSpecial) {
+                ngModel.$validators.minSpecial = minSpecial;
+            }
+
+            if (config.password.minUpper) {
+                ngModel.$validators.minUpper = minUpper;
+            }
         }
 
         function minLength(modelValue, viewValue) {
