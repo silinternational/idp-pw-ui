a60ae8c286d7b422492e82e4e9dd56095b5376df
Billy Clark <billy_clark@sil.org>
2016-12-15 14:25:17 -0500

    users were encountering common issues trying to change/reset password.

--- app/dialog/failedDialog.controller.js
+++ app/dialog/failedDialog.controller.js
@@ -6,19 +6,26 @@
       .controller('FailedDialogController',
                    FailedDialogController);
 
-    function FailedDialogController(dialogService, summary, error) {
+    function FailedDialogController(dialogService, summary, error, $location) {
         var vm = this;
 
         vm.summary = summary;
         vm.error = error;
 
         vm.ok = ok;
+        vm.help = help;
 
         activate();
 
         //////////////////////////////////////////////////////////////////
 
         function activate() {
+        }
+
+        function help() {
+            dialogService.close();
+            
+            $location.url('help');
         }
 
         function ok() {
